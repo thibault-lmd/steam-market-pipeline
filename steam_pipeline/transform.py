fb4d5cd9ec7e6c48fca72ec1d928086dd07bcbfd
Thibault Lam-Detrait <thibault.lamdetrait@gmail.com>
2026-08-24 15:07:02 +0200

    parse_release_date added

--- steam_pipeline/transform.py
+++ steam_pipeline/transform.py
@@ -1,6 +1,8 @@
 """Parser for the JSON file given by the Steam API, and gives readable lines for the database."""
 
-dic = {
+from datetime import date
+
+MONTHS = {
     "mai": 5,
     "aug": 8,
     }
@@ -8,13 +10,21 @@ dic = {
 def parse_release_date(date_str, coming_soon=False):
     """Parse the release date string from the Steam API into a date object."""
 
+    if coming_soon:
+        return None
+    
+
     without_commas = date_str.replace(",", "")
     without_commas = without_commas.lower()
     words = without_commas.split()
+    day = int(words[0])
+    month = MONTHS.get(words[1], None)
+    if month is None:
+        return None
+    year = int(words[2])
+    return date(year, month, day)
 
     
 
 
-    if coming_soon:
-        return None
-
+    
