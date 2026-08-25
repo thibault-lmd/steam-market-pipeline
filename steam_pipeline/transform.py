8381839fc316faf90a60cdebd468114c81ae346c
Thibault Lam-Detrait <thibault.lamdetrait@gmail.com>
2026-08-25 09:52:31 +0200

    Removed french months from dic

--- steam_pipeline/transform.py
+++ steam_pipeline/transform.py
@@ -3,18 +3,18 @@
 from datetime import date
 
 MONTHS = {
-    "jan": 1, "janv": 1, "janvier": 1, "january": 1,
-    "fev": 2, "févr": 2, "février": 2, "feb": 2, "february": 2,
-    "mar": 3, "mars": 3, "march": 3,
-    "avr": 4, "avril": 4, "apr": 4, "april": 4,
-    "mai": 5, "may": 5,
-    "juin": 6, "jun": 6, "june": 6,
-    "juil": 7, "juillet": 7, "jul": 7, "july": 7,
-    "aout": 8, "août": 8, "aug": 8, "august": 8,
-    "sep": 9, "sept": 9, "septembre": 9, "september": 9,
-    "oct": 10, "octobre": 10, "october": 10,
-    "nov": 11, "novembre": 11, "november": 11,
-    "dec": 12, "déc": 12, "décembre": 12, "december": 12,
+    "jan": 1, "january": 1,
+    "feb": 2, "february": 2,
+    "mar": 3, "march": 3,
+    "apr": 4, "april": 4,
+    "may": 5,
+    "jun": 6, "june": 6,
+    "jul": 7, "july": 7,
+    "aug": 8, "august": 8,
+    "sep": 9, "sept": 9, "september": 9,
+    "oct": 10, "october": 10,
+    "nov": 11, "november": 11,
+    "dec": 12, "december": 12,
 }
 
 def parse_release_date(date_str, coming_soon=False):
